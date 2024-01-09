480ff7cb39ad861ad352ed4c76106601ea379c35
ARN <nugrohoandyreza@gmail.com>
2024-01-09 21:33:41 +0700

    git tugas

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
